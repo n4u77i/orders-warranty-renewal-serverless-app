8b0a75c1d4353d33e0e7e811ef0e79c4e5609e7e
n4u77i <nautaxhahmad125@gmail.com>
2022-12-19 14:16:25 +0500

    Added lambda for sending order Update lambda required for iamRole permissions Handler needs to be implemented

--- serverless/functions.ts
+++ serverless/functions.ts
@@ -15,6 +15,47 @@ const functions: AWS['functions'] = {
                 }
             }
         ]
+    },
+
+    sendOrder: {
+        // Lambda function path
+        handler: 'src/functions/sendOrder/index.handler',
+
+        // Event to trigger lambda function
+        events: [
+            {
+                /**
+                 * Stream input event type
+                 * https://www.serverless.com/framework/docs/providers/aws/events/streams
+                 */
+                stream: {
+                    type: 'dynamodb',
+
+                    // Setting stream ARN for dynamoDB dynamically by getting attribute of dynamodb <orderTable>
+                    arn: {
+                        // Getting StreamArn attribute from <orderTable> defined in dynamoResources.ts
+                        "Fn::GetAtt": [
+                            'orderTable', 
+                            'StreamArn'
+                        ]
+                    },
+
+                    /**
+                     * Lambda will only get triggered when the REMOVE event will happen (record will be deleted)
+                     * There are three types of events:
+                     * INSERT: When new item is added to the table
+                     * MODIFY: One or more of an existing item's attributes are modified
+                     * REMOVE: When the item is deleted from the table
+                     * Ref: https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_streams_Record.html
+                     * 
+                     * Usage in Serverless: https://www.serverless.com/framework/docs/providers/aws/events/streams#setting-filter-patterns
+                     */
+                    filterpatterns: {
+                        eventName: ['REMOVE']
+                    }
+                }
+            }
+        ]
     }
 }
 
